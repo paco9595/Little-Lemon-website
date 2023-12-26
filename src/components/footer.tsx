3286e3d5dee6f29ef0031210e1df7d4550941008
paco9595 <paco.villarreal9555@gmail.com>
2023-12-26 12:27:29 -0600

    footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="text-center h-14 mt-10 flex items-center justify-center">
+      <div>copy right (c)</div>
+    </footer>
+  );
+}
